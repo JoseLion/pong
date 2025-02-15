9be677f47bb98d73fd31831cd665252b35575250
Jose Luis Leon <joseluis5000l@gmail.com>
2025-02-14 22:25:50 -0500

    feat(core): Edge collision (#6)

--- src/artifacts/ball.cpp
+++ src/artifacts/ball.cpp
@@ -1,13 +1,22 @@
 #include "ball.h"
+#include "raylib.h"
 
 Ball::Ball(const int& cx, const int& cy): cx(cx), cy(cy) {
 }
 
 const void Ball::draw() const {
-  DrawCircle(this->cx, this->cy, this->SIZE, this->FILL);
+  DrawCircle(cx, cy, r, WHITE);
 }
 
 const void Ball::update() {
-  this->cx += this->speed;
-  this->cy += this->speed;
+  cx += vx;
+  cy += vy;
+
+  if (cx + r >= GetScreenWidth() || cx - r <= 0) {
+    vx *= -1;
+  }
+
+  if (cy + r >= GetScreenHeight() || cy - r <= 0) {
+    vy *= -1;
+  }
 }
